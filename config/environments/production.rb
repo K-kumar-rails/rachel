3578121770ff2d5beffb0179d384947999769798
Mukesh <mpatel.rails@gmail.com>
2013-05-11 07:22:15 +0530

    some changes in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Cat::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'staging-cat.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'rachel-staging.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   # Enable threaded mode
@@ -63,13 +63,13 @@ Cat::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD']
-  }
+  #ActionMailer::Base.smtp_settings = {
+   # :address        => "smtp.sendgrid.net",
+    #:port           => "25",
+    #:authentication => :plain,
+    #:user_name      => ENV['SENDGRID_USERNAME'],
+    #:password       => ENV['SENDGRID_PASSWORD']
+  #}
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
